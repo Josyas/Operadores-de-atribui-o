e8f29762a61df7a6331a98e6f4f667d72185288c
Josias <87546094+Josyas@users.noreply.github.com>
2021-09-15 22:27:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Operadores_de_atribuição {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 10;
+            int b = a++;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+            //Faz a concatenação//
+
+
+        }
+    }
+}
